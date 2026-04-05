debe7414cc1060e77b44b52d9e80fc9e9822ac00
gaurav <gaurav@gaurav>
2026-04-05 05:35:03 +0530

    made some important changes in the file like adding of env variables

--- client/src/useSocket.js
+++ client/src/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState, useCallback } from 'react';
 import { io } from 'socket.io-client';
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000';
+const SERVER_URL = process.env.REACT_APP_SERVER_URL ;
 
 export function useSocket() {
   const socketRef = useRef(null);
